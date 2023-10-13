a713c736eace29f082b383d975c297de18aa08f6
sam.zheng <sam.zheng@funcsync.com>
2023-10-13 17:27:14 +0800

    SW -> 404 頁面

--- sw.js
+++ sw.js
@@ -9,11 +9,22 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
     
     // 攔截到我想要攔截的圖片，並且回傳我想要的圖片
-    if (/cute1.jpg$/.test(event.request.url)) {
-        return event.respondWith(fetch("/cute2.jpg"))
-    }
+    // if (/cute1.jpg$/.test(event.request.url)) {
+    //     // return event.respondWith(fetch("/cute2.jpg"))
+    // }
 
-    
+    // 客製化 404 頁面
+    if (event.request.mode === 'navigate') {
+        return event.respondWith(
+            fetch(event.request).then(response => {
+                if (response.status === 404) {
+                    return fetch('/404.html');
+                }
+
+                return response;
+            })
+        );
+    }
 });
 
 
